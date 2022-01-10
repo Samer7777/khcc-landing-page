019fb02dcfcd465e3fd29d59e458259258935420
SAMER MAJID AL-BAHABSA <62020378+Samer7777@users.noreply.github.com>
2022-01-10 13:58:38 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
-let nums = document.querySelectorAll(" .num");
+const counters = document.querySelectorAll(".num");
+const speed = 111;
+
 let section = document.querySelector(".icon_counter");
 let started = false; // Function Started ? No
-
 window.onscroll = function () {
   if (window.scrollY >= section.offsetTop - 100) {
     if (!started) {
-      nums.forEach((num) => startCount(num));
+      counters.forEach((counter) => {
+        const updateCount = () => {
+          const target = parseInt(+counter.getAttribute("data-goal"));
+          const count = parseInt(+counter.innerText);
+          const increment = Math.trunc(target / speed);
+          console.log(increment);
+          if (count < target) {
+            counter.innerText = count + increment;
+            setTimeout(updateCount, 1);
+          } else {
+            count.innerText = target;
+          }
+        };
+        updateCount();
+      });
     }
     started = true;
   }
 };
-
-function startCount(el) {
-  let goal = el.dataset.goal;
-  let count = setInterval(() => {
-    el.textContent++;
-    if (el.textContent == goal) {
-      clearInterval(count);
-    }
-  }, 100 / goal);
-}
